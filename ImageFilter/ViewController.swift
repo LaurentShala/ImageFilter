66631f80e4f82ca118b06dd64ba4c9c21c28fc2f
LaurentShala <laurentshala@icloud.com>
2016-10-20 19:52:42 -0400

    add save funtionality and fixed rotation bug

--- ImageFilter/ViewController.swift
+++ ImageFilter/ViewController.swift
@@ -12,10 +12,65 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     @IBOutlet var photoImageView: UIImageView!
     
-    @IBAction func applyFilterButton(_ sender: UIButton) {
+    
+    /// Applies a blur effect to the image
+    ///
+    /// - parameter inputImage: UIImage that you with to apply the blur effect to
+    ///
+    /// - returns: UIImage with the blur effect
+    func imageBlur(inputImage: UIImage) -> UIImage {
+        let context = CIContext(options: nil)
         
+        let inputCIImage = CIImage(image: inputImage)!
+        
+        if let blurFilter = CIFilter(name: "CIGaussianBlur") {
+            blurFilter.setValue(inputCIImage, forKey: kCIInputImageKey)
+            blurFilter.setValue(8, forKey: kCIInputRadiusKey)
+                    
+            if let output = blurFilter.outputImage {
+                if let cgimg = context.createCGImage(output, from: output.extent) {
+                    let processedImage = UIImage(cgImage: cgimg)
+                    return processedImage
+                }
+            }
+        }
+        return UIImage()
     }
     
+    /// Applies a blur effect to the image
+    ///
+    /// - parameter inputImage: UIImage that you with to apply the blur effect to
+    ///
+    /// - returns: UIImage with the blur effect
+    func imageCrystallize(inputImage: UIImage) -> UIImage {
+        
+        let context = CIContext(options: nil)
+        let inputCIImage = CIImage(image: inputImage)!
+        
+        if let filter = CIFilter(name: "CICrystallize") {
+            filter.setValue(inputCIImage, forKey: kCIInputImageKey)
+            filter.setValue(55, forKey: kCIInputRadiusKey)
+            
+            if let output = filter.outputImage {
+                if let cgimg = context.createCGImage(output, from: output.extent) {
+                    let processedImage = UIImage(cgImage: cgimg)
+                    return processedImage
+                }
+            }
+        }
+        return UIImage()
+    }
+    
+    @IBAction func saveImageBarButton(_ sender: UIBarButtonItem) {
+        if let imageToBeSaved = photoImageView.image {
+            UIImageWriteToSavedPhotosAlbum(imageToBeSaved, nil, nil, nil)
+        }
+    }
+    
+   
+    @IBAction func applyFilterButton(_ sender: UIButton) {
+        photoImageView.image = imageCrystallize(inputImage: photoImageView.image!)
+    }
     
     
     @IBAction func selectPhotoButton(_ sender: UIButton) {
@@ -23,7 +78,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = .photoLibrary
-            imagePicker.allowsEditing = false
+            imagePicker.allowsEditing = true
             imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
 
             present(imagePicker, animated: true, completion: nil)
